c77eb86d6322902f8158e645804fcaaa2e67754a
roykim1990 <roy.kim@modelop.com>
2023-02-14 18:02:25 -0700

    fix python and job_json

--- rob.py
+++ rob.py
@@ -1,18 +1,60 @@
-import json
+from modelop.monitors.assertions import check_input_types
+from pathlib import Path
+import modelop.schema.infer as infer
 import pandas as pd
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+BINS = []
+BUCKET_COL = ''
+POSITIVE_LABEL = 1
+LABEL_COLUMN = None
 
 # modelop.init
-def init():
+def init(init_param):
     global BINS
     global BUCKET_COL
-    global LABEL_COL
     global POSITIVE_LABEL
+    global LABEL_COLUMN
 
-	# job = json.loads(init_param["rawJson"])
+    job_json = init_param
+
     BINS = [300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]
     BUCKET_COL= "ally_score"
-    LABEL_COL = "defaulted"
     POSITIVE_LABEL = 1
+
+    if job_json is not None:
+        logger.info(
+            "Parameter 'job_json' is present and will be used to extract "
+            "'label_column' and 'score_column'."
+        )
+        input_schema_definition = infer.extract_input_schema(job_json)
+        monitoring_parameters = infer.set_monitoring_parameters(
+            schema_json=input_schema_definition, check_schema=True
+        )
+        LABEL_COLUMN = monitoring_parameters['label_column']
+    else:
+        logger.info(
+            "Parameter 'job_json' it not present, attempting to use "
+            "'label_column' and 'score_column' instead."
+        )
+        if LABEL_COLUMN is None:
+            missing_args_error = (
+                "Parameter 'job_json' is not present,"
+                " but 'label_column'. "
+                "'label_column' input parameter is"
+                " required if 'job_json' is not provided."
+            )
+            logger.error(missing_args_error)
+            raise Exception(missing_args_error)
+    check_input_types(
+        inputs=[
+            {"label_column": LABEL_COLUMN}
+        ],
+        types=(str),
+    )
 
 # modelop.metrics
 def metrics(
@@ -32,16 +74,25 @@ def metrics(
     return {'Rank_Order': list_of_values}
 
 def main():
-    init()
+    raw_json = Path('./example_job.json').read_text()
+    init_param = {'rawJson': raw_json}
+    init(init_param)
+    print('initialized parameters from job_json.')
+    print(BINS)
+    print(BUCKET_COL)
+    print(LABEL_COLUMN)
+    print(POSITIVE_LABEL)
     data = pd.read_csv('./rob_test.csv')
+    print('read data.')
     result = metrics(
             data,
             BINS,
             BUCKET_COL,
-            LABEL_COL,
+            LABEL_COLUMN,
             POSITIVE_LABEL
         )
     print(json.dumps(result, indent=3, sort_keys=True))
-
+    print('done.')
+    
 if __name__ == '__main__':
 	main()
